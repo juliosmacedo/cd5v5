eed728e9157b0d0fac3b32f1716e636fd00be9f0
Rafael Abensur <rafael@abensur.me>
2022-09-21 20:50:40 -0300

    feat: gh page

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
 
 export default defineConfig({
-  // base: 'https://cozinhadoscinco.com.br',
+  base: '/cd5v5/',
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
