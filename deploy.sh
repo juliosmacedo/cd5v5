3a5122d329b0493a37032852341e4fc41ca7bf4c
Rafael Abensur <rafael@abensur.me>
2022-09-21 20:43:24 -0300

    feat: gh page

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'www.cozinhadoscinco.com' > CNAME
+# echo 'www.cozinhadoscinco.com' > CNAME
 
 git init
 git checkout -b main
